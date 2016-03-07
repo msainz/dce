2f25bbb17c9fa08ef7e3bd4f85e34c5ef6216680
SergioVM <serteckian@gmail.com>
2016-03-07 21:29:16 +0100

    added some test units that show how the compute quantile rutine fails

--- test/es/valcarcelsainz/dce/DCEAgentTest.java
+++ test/es/valcarcelsainz/dce/DCEAgentTest.java
@@ -36,7 +36,6 @@ public class DCEAgentTest {
                 () -> ArrayUtils.toPrimitive(iterator1.next()),
                 (double[] x) -> x[0],
                 gammaQuantile);
-
         assertEquals(0., gamma, 1e-14);
 
         /*
@@ -49,7 +48,6 @@ public class DCEAgentTest {
                 () -> ArrayUtils.toPrimitive(iterator2.next()),
                 (double[] x) -> x[0],
                 gammaQuantile);
-
         assertEquals(1., gamma, 1e-14);
 
         /*
@@ -62,7 +60,45 @@ public class DCEAgentTest {
                 () -> ArrayUtils.toPrimitive(iterator3.next()),
                 (double[] x) -> x[0],
                 gammaQuantile);
-
         assertEquals(9., gamma, 1e-14);
+
+
+       /*
+         * example 4
+         */
+        gammaQuantile = 0.5;
+        Double[][] example4 = new Double[][] { {0.0}, {0.1}, {0.2}, {0.3}, {0.4}, {0.5}, {0.6}, {0.7}, {0.8}, {0.9} };
+        Iterator<Double[]> iterator4 = Arrays.asList(example4).iterator();
+        gamma = DCEAgent.computeGamma(new double[10][1], new double[10],
+                () -> ArrayUtils.toPrimitive(iterator4.next()),
+                (double[] x) -> x[0],
+                gammaQuantile);
+        //assertEquals(0.5, gamma, 1e-14);
+
+
+        /*
+         * example 5
+         */
+        gammaQuantile = 0.2;
+        Double[][] example5= new Double[][] { {0.0}, {0.01}, {0.02}, {0.03}, {0.04} };
+        Iterator<Double[]> iterator5 = Arrays.asList(example5).iterator();
+        gamma = DCEAgent.computeGamma(new double[5][1], new double[5],
+                () -> ArrayUtils.toPrimitive(iterator5.next()),
+                (double[] x) -> x[0],
+                gammaQuantile);
+        //assertEquals(0.01, gamma, 1e-14);
+
+
+        /*
+         * example 6
+         */
+        gammaQuantile = 0.7;
+        Double[][] example6= new Double[][] { {0.0}, {0.01}, {0.02}, {0.03}, {0.04}, {0.05}, {0.06}, {0.07}, {0.08}, {0.09}  };
+        Iterator<Double[]> iterator6 = Arrays.asList(example6).iterator();
+        gamma = DCEAgent.computeGamma(new double[10][1], new double[10],
+                () -> ArrayUtils.toPrimitive(iterator6.next()),
+                (double[] x) -> x[0],
+                gammaQuantile);
+        assertEquals(0.07, gamma, 1e-14);
     }
 }
